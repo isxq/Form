2359eba0c3588b7a5726502240c479a07d5b052f
isxq <shen_x_q@163.com>
2018-12-23 19:35:12 +0800

    Declarative Syntax

--- Form/Form/Form.swift
+++ Form/Form/Form.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+typealias Element<El, A> = (RenderingContext<A>) -> RenderedElement<El, A>
+typealias Form<A> = Element<[Section], A>
+typealias RenderedSection<A> = Element<Section, A>
+
 final class TargetAction {
     let excute: ()->Void
     init(_ excute: @escaping ()->Void) {
@@ -54,17 +58,22 @@ class FormDriver<State> {
     var state : State {
         didSet {
             render.update(state)
-            formViewController.reloadSectionFooters()
+            formViewController.reloadSections()
         }
     }
 }
 
-class Section {
+class Section: Equatable {
+    
     let cells: [FormCell]
     var footerTitle: String?
     init(cells: [FormCell], footerTitle: String?) {
         self.cells = cells
         self.footerTitle = footerTitle
+    }
+    
+    static func == (lhs: Section, rhs: Section) -> Bool {
+        return lhs === rhs
     }
 }
 
@@ -94,8 +103,7 @@ class FormViewController: UITableViewController {
         firstResponder?.becomeFirstResponder()
     }
     
-    func reloadSectionFooters() {
-        UIView.setAnimationsEnabled(false)
+    func reloadSections() {
         tableView.beginUpdates()
         for index in sections.indices {
             let footerView = tableView.footerView(forSection: index)
@@ -103,7 +111,6 @@ class FormViewController: UITableViewController {
             footerView?.setNeedsLayout()
         }
         tableView.endUpdates()
-        UIView.setAnimationsEnabled(true)
     }
     
     
@@ -140,62 +147,120 @@ class FormViewController: UITableViewController {
     
 }
 
-func uiswitich<State>(context: RenderingContext<State>, keyPath: WritableKeyPath<State, Bool>) -> RenderedElement<UIView, State> {
-    
-    let toggle = UISwitch()
-    toggle.translatesAutoresizingMaskIntoConstraints = false
-    let toggleTarget = TargetAction {
-        context.change{$0[keyPath: keyPath] = toggle.isOn}
+func uiswitich<State>(keyPath: WritableKeyPath<State, Bool>) -> Element<UIView, State> {
+    return { context in
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        let toggleTarget = TargetAction {
+            context.change{$0[keyPath: keyPath] = toggle.isOn}
+        }
+        toggle.addTarget(toggleTarget, action: #selector(toggleTarget.action(_:)), for: .valueChanged)
+        return RenderedElement(element: toggle, strongRefrences: [toggleTarget], update: { state in
+            toggle.isOn = state[keyPath: keyPath]
+        })
     }
-    toggle.addTarget(toggleTarget, action: #selector(toggleTarget.action(_:)), for: .valueChanged)
-    return RenderedElement(element: toggle, strongRefrences: [toggleTarget], update: { state in
-        toggle.isOn = state[keyPath: keyPath]
-    })
 }
 
 
-func textField<State>(context: RenderingContext<State>, keyPath: WritableKeyPath<State, String>) -> RenderedElement<UIView, State> {
-    
-    let textfield = UITextField()
-    textfield.translatesAutoresizingMaskIntoConstraints = false
-    
-    let didEnd = TargetAction {
-        context.change{ $0[keyPath: keyPath] = textfield.text ?? "" }
+func textField<State>(keyPath: WritableKeyPath<State, String>) -> Element<UIView, State> {
+    return { context in
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        
+        let didEnd = TargetAction {
+            context.change{ $0[keyPath: keyPath] = textfield.text ?? "" }
+        }
+        
+        let didExit = TargetAction {
+            context.change{ $0[keyPath: keyPath] = textfield.text ?? "" }
+            context.popViewController()
+        }
+        
+        textfield.addTarget(didEnd, action: #selector(didEnd.action(_:)), for: .editingDidEnd)
+        textfield.addTarget(didExit, action: #selector(didExit.action(_:)), for: .editingDidEndOnExit)
+        return RenderedElement(element: textfield, strongRefrences:[didEnd, didExit], update: { state in
+            textfield.text = state[keyPath: keyPath]
+        })
     }
-    
-    let didExit = TargetAction {
-        context.change{ $0[keyPath: keyPath] = textfield.text ?? "" }
-        context.popViewController()
-    }
-    
-    textfield.addTarget(didEnd, action: #selector(didEnd.action(_:)), for: .editingDidEnd)
-    textfield.addTarget(didExit, action: #selector(didExit.action(_:)), for: .editingDidEndOnExit)
-    return RenderedElement(element: textfield, strongRefrences:[didEnd, didExit], update: { state in
-        textfield.text = state[keyPath: keyPath]
-    })
 }
 
-func controlCell<State>(title: String, control: RenderedElement<UIView, State>, leftAligned: Bool = false)-> RenderedElement<FormCell, State> {
-    let cell = FormCell(style: .value1, reuseIdentifier: nil)
-    cell.contentView.addSubview(control.element)
-    cell.textLabel?.text = title
-    cell.contentView.addConstraints([
-        control.element.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
-        control.element.trailingAnchor.constraint(equalTo: cell.contentView.layoutMarginsGuide.trailingAnchor)
-        ])
-    if leftAligned {
-        cell.contentView.addConstraint( control.element.leadingAnchor.constraint(equalTo: cell.textLabel!.trailingAnchor, constant: 20))
+func controlCell<State>(title: String, control: @escaping Element<UIView, State>, leftAligned: Bool = false)-> Element<FormCell, State> {
+    return { context in
+        let renderedControl = control(context)
+        let cell = FormCell(style: .value1, reuseIdentifier: nil)
+        cell.contentView.addSubview(renderedControl.element)
+        cell.textLabel?.text = title
+        cell.contentView.addConstraints([
+            renderedControl.element.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+            renderedControl.element.trailingAnchor.constraint(equalTo: cell.contentView.layoutMarginsGuide.trailingAnchor)
+            ])
+        if leftAligned {
+            cell.contentView.addConstraint( renderedControl.element.leadingAnchor.constraint(equalTo: cell.textLabel!.trailingAnchor, constant: 20))
+        }
+        return RenderedElement(element: cell, strongRefrences: renderedControl.strongRefrences, update: renderedControl.update)
     }
-    return RenderedElement(element: cell, strongRefrences: control.strongRefrences, update: control.update)
 }
 
-func detailTextCell<State>(title: String, keyPath: KeyPath<State, String>, didSelect: @escaping () -> ()) -> RenderedElement<FormCell, State> {
-    let cell = FormCell(style: .value1, reuseIdentifier: nil)
-    cell.didSelect = didSelect
-    cell.textLabel?.text = title
-    cell.accessoryType = .disclosureIndicator
-    cell.shouldHighlight = true
-    return RenderedElement(element: cell, strongRefrences: [], update: { state in
-        cell.detailTextLabel?.text = state[keyPath: keyPath]
-    })
+func optionCell<Input: Equatable, State>(title: String, option: Input, keyPath: WritableKeyPath<State, Input>)->Element<FormCell, State> {
+    return { context in
+        let cell = FormCell(style: .value1, reuseIdentifier: nil)
+        cell.textLabel?.text = title
+        cell.shouldHighlight = true
+        cell.didSelect = {
+            context.change { $0[keyPath: keyPath] = option }
+        }
+        return RenderedElement.init(element: cell, strongRefrences: [], update: { state in
+            cell.accessoryType = state[keyPath: keyPath] == option ? .checkmark : .none
+        })
+        
+    }
+}
+
+func detailTextCell<State>(title: String, keyPath: KeyPath<State, String>, form: @escaping Element<[Section], State>) -> Element<FormCell, State> {
+    return { context in
+        let cell = FormCell(style: .value1, reuseIdentifier: nil)
+        cell.shouldHighlight = true
+        cell.textLabel?.text = title
+        cell.accessoryType = .disclosureIndicator
+        let renderedForm = form(context)
+        let nested = FormViewController(sections:renderedForm.element, title: title)
+        cell.didSelect = {
+            context.pushViewController(nested)
+        }
+        
+        return RenderedElement(element: cell, strongRefrences: [], update: { state in
+            cell.detailTextLabel?.text = state[keyPath: keyPath]
+            renderedForm.update(state)
+            nested.reloadSections()
+        })
+    }
+}
+
+func section<State>(_ cells: [Element<FormCell, State>], footer keyPath: KeyPath<State, String?>? = nil) -> RenderedSection<State> {
+    return { context in
+        let renderedCells = cells.map{ $0(context) }
+        let section = Section(cells: renderedCells.map{$0.element}, footerTitle: nil)
+        return RenderedElement(
+            element: section,
+            strongRefrences: renderedCells.flatMap{$0.strongRefrences},
+            update: { state in
+                renderedCells.forEach{$0.update(state)}
+                if let kp = keyPath {
+                    section.footerTitle = state[keyPath: kp]
+                }
+        })
+    }
+}
+
+func sections<State>(_ sections: [RenderedSection<State>]) -> Form<State> {
+    return { context in
+        let renderedSections = sections.map{$0(context)}
+        return RenderedElement.init(
+            element: renderedSections.map{$0.element},
+            strongRefrences: renderedSections.flatMap{$0.strongRefrences},
+            update: { state in
+                renderedSections.forEach{$0.update(state)
+                }
+        })
+    }
 }
--- Form/Form/FormViewDriver.swift
+++ Form/Form/FormViewDriver.swift
@@ -19,59 +19,19 @@ extension Hotspot {
     }
 }
 
-func section<State>(_ renderedCells: [RenderedElement<FormCell, State>], footer keyPath: KeyPath<State, String?>? = nil) -> RenderedElement<Section, State> {
-    let section = Section(cells: renderedCells.map{$0.element}, footerTitle: nil)
-    return RenderedElement(
-        element: section,
-        strongRefrences: renderedCells.flatMap{$0.strongRefrences},
-        update: { state in
-            renderedCells.forEach{$0.update(state)}
-            if let kp = keyPath {
-                section.footerTitle = state[keyPath: kp]
-            }
-    })
-}
+let passwordForm: Form<Hotspot> =
+    sections([
+        section([
+            controlCell(title: "Password", control: textField(keyPath: \.password))
+        ])
+    ])
 
-func sections<State>(_ renderedSections: [RenderedElement<Section, State>]) -> RenderedElement<[Section], State> {
-    return RenderedElement.init(
-        element: renderedSections.map{$0.element},
-        strongRefrences: renderedSections.flatMap{$0.strongRefrences},
-        update: { state in
-            renderedSections.forEach{$0.update(state)}
-    })
-}
-
-func hotspotForm(context: RenderingContext<Hotspot>) -> RenderedElement<[Section], Hotspot> {
-    let renderedToggle = uiswitich(context: context, keyPath: \Hotspot.isEnabled)
-    let renderedToggleCell = controlCell(title: "Personal Hotspot", control: renderedToggle)
-    
-    let renderedPasswordForm = buildPasswordForm(context: context)
-    let nested = FormViewController(sections: renderedPasswordForm.element, title: "Personal Hotspot Password")
-    
-    let passwordCell = detailTextCell(title: "Password", keyPath: \Hotspot.password) {
-        context.pushViewController(nested)
-    }
-    
-    let toggleSection = section([renderedToggleCell])
-    //    updates.append { state in
-    //        toggleSection.footerTitle = state.enabledSectionTitle
-    //    }
-    
-    let passwordSection = section([passwordCell])
-    
-    // TODO: renderedPasswordForm.strongReferences,
-    // renderedPasswordForm.update(state)
-    
-    return sections([toggleSection, passwordSection])
-    
-    
-    
-}
-
-func buildPasswordForm(context: RenderingContext<Hotspot>) -> RenderedElement<[Section], Hotspot>{
-    
-    let renderedPasswordField = textField(context: context, keyPath: \.password)
-    let passwordCell = controlCell(title: "Password", control: renderedPasswordField, leftAligned: true)
-    return sections([section([passwordCell])])
-}
-
+let hotspotForm: Form<Hotspot> =
+    sections([
+        section([
+            controlCell(title: "Personal Hotspot", control: uiswitich(keyPath: \.isEnabled))
+            ], footer: \.enableSectionTitle),
+        section([
+            detailTextCell(title: "Password", keyPath: \.password, form: passwordForm)
+            ])
+        ])
